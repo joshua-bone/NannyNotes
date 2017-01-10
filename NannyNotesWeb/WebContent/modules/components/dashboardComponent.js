c7b53dcf28c42afcd98dd98523fb2e6e8441b37b
Sarah Passantino <spassantino@yahoo.com>
2017-01-10 13:54:45 -0700

    dashboard looking prettier

--- NannyNotesWeb/WebContent/modules/components/dashboardComponent.js
+++ NannyNotesWeb/WebContent/modules/components/dashboardComponent.js
@@ -2,59 +2,38 @@ angular.module("NannyNotesApp")
 .component('dashboardComponent', {
 	controller : function() {
 	    var vm = this;
+	    /* off-canvas sidebar toggle */
+	    $('[data-toggle=offcanvas]').click(function() {
+	        $('.row-offcanvas').toggleClass('active');
+	        $('.collapse').toggleClass('in').toggleClass('hidden-xs').toggleClass('visible-xs');
+	    });
 	  },
 	 template : `
-	 <div class="container">
-	<h3>Sexy Sidebar Navigation<br />
-        <small>A simple, sheek navigation bar style!</small>
-    </h3>
-    <br />
-    
-    <div class="row">
-        <div class="col-sm-2">
-            <nav class="nav-sidebar">
-                <ul class="nav">
-                    <li class="active"><a href="javascript:;">Home</a></li>
-                    <li><a href="javascript:;">About</a></li>
-                    <li><a href="javascript:;">Products</a></li>
-                    <li><a href="javascript:;">FAQ</a></li>
-                    <li class="nav-divider"></li>
-                    <li><a href="javascript:;"><i class="glyphicon glyphicon-off"></i> Sign in</a></li>
-                </ul>
-            </nav>
+<div class="wrapper">
+    <div class="row row-offcanvas row-offcanvas-left">
+        <!-- sidebar -->
+        <div class="column col-sm-3 col-xs-1 sidebar-offcanvas" id="sidebar">
+            <ul class="nav" id="menu">
+                <li><a href="#"><i class="fa fa-list-alt"></i> <span class="collapse in hidden-xs">Household</span></a></li>
+                <li>
+                
+                    <a data-target="#item1" data-toggle="collapse"><i class="fa fa-list"></i> <span class="collapse in hidden-xs">Schedule <span class="caret"></span></span></a>
+                    <ul class="nav nav-stacked collapse left-submenu" id="item1">
+                        <li><a href="google.com">Daily</a></li>
+                        <li><a href="gmail.com">Weekly</a></li>
+		 				<li><a href="gmail.com">Monthly</a></li>
+                    </ul>
+                </li>
+		 <li><a href="#"><i class="fa fa-paperclip"></i> <span class="collapse in hidden-xs">Notes</span></a></li>
+            </ul>
         </div>
-        <div class="col-sm-2 col-sm-offset-8">
-        <nav class="nav-sidebar pull-right">
-                <ul class="nav">
-                    <li class="active"><a href="javascript:;">Home</a></li>
-                    <li><a href="javascript:;">A long link will naturally wrap around</a></li>
-                    <li class="text-overflow" title="An long link with .text-overflow"><a href="javascript:;">A link with .text-overflow can be applied in case it's too long!</a></li>
-                    <li>
-                        <a href="javascript:;" class="media">
-                            <span class="pull-left">
-                                <i class="media-object glyphicon glyphicon-list"></i>
-                            </span>
-                            <div class="media-body">
-                                A media object can also be a link, making longer wrapping links play well with the icons!
-                            </div>
-                        </a>
-                    </li>
-                    <li class="text-overflow" title="Combine them all to make an awesome, safe solution!">
-                        <a href="javascript:;" class="media">
-                            <span class="pull-left">
-                                <i class="media-object glyphicon glyphicon-list"></i>
-                            </span>
-                            <div class="media-body">
-                                Combine them all to make an awesome, safe solution!
-                            </div>
-                        </a>
-                    </li>
-                    <li class="nav-divider"></li>
-                    <li><a href="javascript:;"><i class="glyphicon glyphicon-off"></i> Sign in</a></li>
-                </ul>
-            </nav>
-        </div>
-    </div>
-</div>
+        <!-- /sidebar -->
+
+        <!-- main right col -->
+        <div class="column col-sm-9 col-xs-11" id="main">
+            <p><a href="#" data-toggle="offcanvas"><i class="fa fa-navicon fa-2x"></i></a></p>
+            <p>
+                Main content...
+            </p>
 		 		`
 });
