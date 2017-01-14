a53f07957fcc5fc7a344bc4be680699e195b6d6d
Sarah Passantino <spassantino@yahoo.com>
2017-01-14 11:12:33 -0700

    added notes template

--- NannyNotesWeb/WebContent/modules/components/notesComponent.js
+++ NannyNotesWeb/WebContent/modules/components/notesComponent.js
@@ -1,19 +1,56 @@
 angular.module("NannyNotesApp")
 .component('notesComponent', {
-	controller : function() {
-	    var vm = this;
+	controller : function(authenticationService, userService, householdService) {
+		  var vm = this;
+
+		    vm.getUser = function(id){
+		    	userService.getUser(currentUser.id)
+		    	.then(function(response){
+		    		vm.user =response.data;
+		    		console.log(response.data);
+		    		console.log('in get user component function');
+		    	}).catch(function(err){
+		    		console.log('in get user component error');
+		    	});
+		    }
+
+		  vm.destroyHousehold = function(id) {
+	    	householdService.deleteHousehold(id)
+	    	.then(function(response){
+	    		vm.households = response.data;
+	    		vm.loadHouseholds();
+	    		console.log("in users component calling household service");
+	    	}).catch(function(err){
+	    		console.log('in destroy error');
+	    	});
+	    };
+
+	    vm.editHousehold = function(household) {
+	    	householdService.updateHousehold(household)
+	    	.then(function(response){
+	    		vm.household = response.data;
+
+	    		console.log("in users component calling household service");
+	    	}).catch(function(err){
+	    		console.log('in edit error');
+	    	});
+	    };
+
+	    vm.getChildren = function(household){
+	    	householdService.getHouseholdChildren(household)
+	    	.then(function(response){
+	    		vm.children = response.data;
+	    		console.log(vm.children);
+	    		console.log("in users component calling household service");
+	    	}).catch(function(err){
+	    		console.log('in getChildren error');
+	    	});
+
+	    }
+			vm.user = userService.getCurrentUser();
+			vm.household = householdService.getCurrentHousehold();
+			vm.children = vm.getChildren(vm.household);
+
 	  },
-	 template : 
-		 		`
-		 		<nav-component></nav-component>
-		 		<dashboard-component></dashboard-component>
-    <!-- main right col -->
-        <div class="column col-sm-9 col-xs-11" id="main">
-            <p><a href="#" data-toggle="offcanvas"><i class="fa fa-navicon fa-2x"></i></a></p>
-            <p>
-                Main content...
-                <h1> NOTES! </h1>
-            </p>
-		 		`
-		 
+		 templateUrl : 'templates/viewUpdateNotes.html'
 });
